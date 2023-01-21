4e5791e3cdd1d9dfd7bbaaf0ca00404d30bd638e
edurevsky <edurevsky@gmail.com>
2023-01-21 16:06:18 -0300

    feat: updates redirector repository

--- src/main/java/dev/edurevsky/urlshortener/core/RedirectorRepository.java
+++ src/main/java/dev/edurevsky/urlshortener/core/RedirectorRepository.java
@@ -4,4 +4,6 @@ public interface RedirectorRepository {
 
     Redirector create(Redirector redirector);
     Redirector findBySlug(Slug slug);
+    boolean slugAreadyExists(Slug slug);
+    void deleteAllExpired();
 }
