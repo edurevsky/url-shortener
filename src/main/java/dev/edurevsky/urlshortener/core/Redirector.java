c14a38783af03db26fdb865d32864c170cdb0769
edurevsky <edurevsky@gmail.com>
2023-01-21 12:02:57 -0300

    feat: adds expiration to redirector

--- src/main/java/dev/edurevsky/urlshortener/core/Redirector.java
+++ src/main/java/dev/edurevsky/urlshortener/core/Redirector.java
@@ -1,13 +1,27 @@
 package dev.edurevsky.urlshortener.core;
 
+import java.time.LocalDateTime;
+
 public final class Redirector {
 
     private Slug slug;
     private String redirectsTo;
+    private LocalDateTime expiration;
+
+    public Redirector() {
+
+    }
 
     public Redirector(String fromUrl) {
         this.slug = new Slug();
         this.redirectsTo = fromUrl;
+        this.expiration = LocalDateTime.now().plusHours(1);
+    }
+
+    public Redirector(Slug slug, String redirectsTo, LocalDateTime expiration) {
+        this.slug = slug;
+        this.redirectsTo = redirectsTo;
+        this.expiration = expiration;
     }
 
     public String getSlug() {
@@ -16,5 +30,13 @@ public final class Redirector {
 
     public String getRedirectsTo() {
         return redirectsTo;
+    }
+
+    public LocalDateTime getExpiration() {
+        return expiration;
+    }
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(this.expiration);
     }
 }
---
+++ src/test/java/dev/edurevsky/urlshortener/core/RedirectorTest.java
@@ -0,0 +1,37 @@
+package dev.edurevsky.urlshortener.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedirectorTest {
+
+    @Test
+    void returnsTrueWhenIsExpired() {
+
+        var slug = new Slug();
+        var url = "https://google.com";
+        var datetime = LocalDateTime.now().minusHours(1L);
+
+        var redirector = new Redirector(slug, url, datetime);
+
+        assertTrue(redirector.isExpired());
+
+    }
+
+    @Test
+    void returnsFalseWhenNotIsExpired() {
+
+        var slug = new Slug();
+        var url = "";
+        var datetime = LocalDateTime.now().plusHours(1L);
+
+        var redirector = new Redirector(slug, url, datetime);
+
+        assertFalse(redirector.isExpired());
+
+    }
+
+}
