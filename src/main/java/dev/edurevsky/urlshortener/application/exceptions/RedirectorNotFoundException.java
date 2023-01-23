e9079aeaeb6c1529f309b68a1f21dd02a9e32c8c
edurevsky <edurevsky@gmail.com>
2023-01-23 19:39:14 -0300

    feat: adds redirector not found exception

---
+++ src/main/java/dev/edurevsky/urlshortener/application/exceptions/RedirectorNotFoundException.java
@@ -0,0 +1,16 @@
+package dev.edurevsky.urlshortener.application.exceptions;
+
+import dev.edurevsky.urlshortener.core.Slug;
+
+public class RedirectorNotFoundException extends RuntimeException {
+
+    private static final String MESSAGE = "Redirector with slug '%s' not found.";
+
+    private RedirectorNotFoundException(Slug slug) {
+        super(MESSAGE.formatted(slug));
+    }
+
+    public static RedirectorNotFoundException from(Slug slug) {
+        return new RedirectorNotFoundException(slug);
+    }
+}
