46c14a2135dde015116bb9fc746d4aec8cdcceca
edurevsky <edurevsky@gmail.com>
2023-01-24 12:05:06 -0300

    feat: adds delete all expired task

---
+++ src/main/java/dev/edurevsky/urlshortener/application/tasks/DeleteAllExpired.java
@@ -0,0 +1,19 @@
+package dev.edurevsky.urlshortener.application.tasks;
+
+import dev.edurevsky.urlshortener.application.commands.NoCommand;
+import dev.edurevsky.urlshortener.core.RedirectorRepository;
+
+public final class DeleteAllExpired extends Task {
+
+    private final RedirectorRepository redirectorRepository;
+
+    public DeleteAllExpired(RedirectorRepository redirectorRepository) {
+        this.redirectorRepository = redirectorRepository;
+    }
+
+    @Override
+    public Void execute(NoCommand ignored) {
+        redirectorRepository.deleteAllExpired();
+        return null;
+    }
+}
