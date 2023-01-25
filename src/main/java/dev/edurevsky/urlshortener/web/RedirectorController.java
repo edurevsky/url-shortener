6275d491cf536f20fc38f6020839a941a2eb3562
edurevsky <edurevsky@gmail.com>
2023-01-25 10:46:41 -0300

    feat: adds redirector controller

---
+++ src/main/java/dev/edurevsky/urlshortener/web/RedirectorController.java
@@ -0,0 +1,44 @@
+package dev.edurevsky.urlshortener.web;
+
+import dev.edurevsky.urlshortener.application.usecases.CreateRedirector;
+import dev.edurevsky.urlshortener.application.usecases.FindRedirector;
+import dev.edurevsky.urlshortener.core.Redirector;
+import dev.edurevsky.urlshortener.web.requests.NewRedirectorRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static org.springframework.http.ResponseEntity.created;
+
+@RestController
+@RequestMapping("/")
+public class RedirectorController {
+
+    private final CreateRedirector createRedirector;
+    private final FindRedirector findRedirector;
+
+    public RedirectorController(
+            CreateRedirector createRedirector,
+            FindRedirector findRedirector
+    ) {
+        this.createRedirector = createRedirector;
+        this.findRedirector = findRedirector;
+    }
+
+    @GetMapping("{slug}")
+    public RedirectView redirect(@PathVariable("slug") String slug) {
+        var redirector = this.findRedirector.execute(() -> slug);
+
+        var url = redirector.getRedirectsTo();
+
+        return new RedirectView(url);
+    }
+
+    @PostMapping
+    public ResponseEntity<Redirector> create(@RequestBody NewRedirectorRequest request, UriComponentsBuilder uriBuilder) {
+        var redirector = this.createRedirector.execute(request);
+        var location = uriBuilder.path("/{slug}").build(redirector.getSlug());
+        return created(location).body(redirector);
+    }
+}
